7453d133cfcad2bc10afbf8f8f881061ae80157a
scurrypf <2549018667@qq.com>
2022-12-23 20:54:36 +0800

    myApply实现

---
+++ JS.prototype/myApply.js
@@ -0,0 +1,11 @@
+Function.prototype.myApply() = function (obj,args){//传入对象与数组
+    if(this===Function.prototype){//防止直接被调用
+        return undefined;
+    }
+    obj = obj || window;
+    Symbol [fun] = Symbol();
+    obj[fun] = this;
+    const result = obj[fun](...args);//数组结构调用
+    delete obj[fun];//调用完delete掉
+    return result;
+}
