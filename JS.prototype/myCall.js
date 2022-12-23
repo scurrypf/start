4111327df74943f875fe94157ca1de9e9fdaae0b
scurrypf <2549018667@qq.com>
2022-12-23 20:47:43 +0800

    myCall实现

---
+++ JS.prototype/myCall.js
@@ -0,0 +1,11 @@
+Function.prototype.myApply() = function (obj,...args){
+    if(this===Function.prototype){
+        return undefined;
+    }
+    obj = obj || window;//obj(对象)为可选参数，在没有赋值时为window
+    Symbol [fun]=Symbol();//使用symbol防止有同名属性
+    obj[fun]=this;
+    const result = obj[fun](...args);//借用该函数并执行
+    delete obj[fun];//借用完函数delete掉
+    return result;//返回函数执行值
+}
