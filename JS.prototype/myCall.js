117c5c4da7b3ab6d1bbbab5c97870bd00a68b506
scurrypf <2549018667@qq.com>
2022-12-23 20:57:31 +0800

    myCall--

--- JS.prototype/myCall.js
+++ JS.prototype/myCall.js
@@ -3,7 +3,7 @@ Function.prototype.myCall() = function (obj,...args){//传入对象与数值
         return undefined;
     }
     obj = obj || window;//obj(对象)为可选参数，在没有赋值时为window
-    Symbol [fun]=Symbol();//使用symbol防止有同名属性
+    const fun = Symbol();//使用symbol防止有同名属性
     obj[fun]=this;
     const result = obj[fun](...args);//借用该函数并执行
     delete obj[fun];//借用完函数delete掉
