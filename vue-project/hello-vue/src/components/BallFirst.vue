950e051cea92994a234e74a8d7aea7c199ea98f4
scurrypf <2549018667@qq.com>
2022-12-29 17:35:41 +0800

    父子踢球数据循环

---
+++ vue-project/hello-vue/src/components/BallFirst.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <BallSecond :ball="firstBall" @second-ball="getSecond"></BallSecond>
+    </div>
+</template>
+
+<script>
+import BallSecond from './BallSecond.vue';
+
+export default{
+    name:'BallFirst',
+    components:{
+        BallSecond,
+    },
+    data:function(){
+        return {
+            firstBall:0,
+        }
+    },
+    methods:{
+        getSecond(v){
+            this.firstBall=v+1;
+            console.log('firstTosecond',this.firstBall);
+        }
+    }
+    
+}
+</script>
---
+++ vue-project/hello-vue/src/components/BallSecond.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default{
+    name:'BallSecond',
+    props:{
+        ball:Number,
+    },
+    watch:{
+        ball:{
+            handler: function(newValue){
+                console.log('休息2s')
+                setTimeout(()=>{
+                    console.log('secondTofirst',newValue+1);
+                    this.$emit('second-ball',newValue+1); 
+                },2000)
+            },
+            immediate:true
+        }
+    }
+}
+</script>
