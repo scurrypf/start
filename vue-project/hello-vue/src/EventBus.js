c12384e88eb541293c75ed3c569d98a4b44977e2
scurrypf <2549018667@qq.com>
2022-12-30 19:54:23 +0800

    事件总线

---
+++ vue-project/hello-vue/src/EventBus.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+const EventBus = new Vue();
+
+const EventType={
+    Data_Type:'Data_Type',
+}
+export{
+    EventBus,EventType
+}
---
+++ vue-project/hello-vue/src/MittEvent.js
@@ -0,0 +1,9 @@
+import Mitt from 'mitt'
+const MittEvent = new Mitt();
+
+const MittEventType={
+    Data_Type:'Data_Type',
+}
+export{
+    MittEvent,MittEventType
+}
