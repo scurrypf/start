f0f0722fddfcedb6917162305eecb762879d8dc3
scurrypf <2549018667@qq.com>
2023-02-14 22:10:29 +0800

    feat:添加了更新数据功能

--- vue-project/router-vue/src/components/MainComp/UserdataComp.vue
+++ vue-project/router-vue/src/components/MainComp/UserdataComp.vue
@@ -7,17 +7,41 @@
             </el-breadcrumb>
         </div>
         <el-divider></el-divider>
-        <el-table :data="tableData" border style="width: 1200px" height="408">
-            <el-table-column fixed prop="username" label="用户名" width="300"></el-table-column>
-            <el-table-column prop="sex" label="性别" width="300"></el-table-column>
-            <el-table-column prop="pass" label="密码" width="300"></el-table-column>
+        <!--   -->
+        <el-table :data="tableData" border style="width: 1200px" height="373" class="tablestyle" stripe>
+            <el-table-column fixed prop="username" label="用户名" width="350"></el-table-column>
+            <el-table-column prop="sex" label="性别" width="350"></el-table-column>
+            <el-table-column prop="pass" label="密码" width="350"></el-table-column>
             <el-table-column label="操作">    
                 <template slot-scope="scope">
-                    <el-button @click="handleClick(scope.row)" type="text" size="medium">编辑</el-button>
-                    <el-button type="text" size="medium" @click="deleteUser(scope.row)" >删除</el-button>
+                    <el-button @click="getUpdate(scope.row)" type="text" size="medium">编辑</el-button>
+                    <el-dialog title="更新用户" :visible.sync="dialogformVisible" :append-to-body="true" boder="false">
+                        <el-form>
+                            <el-form-item label="ID">
+                                {{ id }}
+                            </el-form-item>
+                            <el-form-item label="用户名">
+                                <el-input v-model="name" autocomplete="off"></el-input>
+                            </el-form-item>
+                            <el-form-item label="性别">
+                                <el-radio v-model="sexy" label="男">男</el-radio>
+                                <el-radio v-model="sexy" label="女">女</el-radio>
+                            </el-form-item>
+                            <el-form-item label="密码">
+                                <el-input v-model="password" autocomplete="off"></el-input>
+                            </el-form-item>
+                        </el-form>
+                        <div slot="footer" class="dialog-footer">
+                            <el-button @click="dialogformVisible = false">取 消</el-button>
+                            <el-button type="primary" @click="updateUser">确 定</el-button>
+                        </div>
+                    </el-dialog>
+                    <span style="width: 20px;"></span>
+                    <el-button type="text" size="meadium" @click="deleteUser(scope.row)">删除</el-button>
                 </template>
             </el-table-column>
         </el-table>
+        <el-pagination background layout="prev, pager, next" :total="20"></el-pagination>
         <div>
           <el-button @click="getadd" icon="el-icon-plus" type="primary" class="add" plain>添加用户</el-button>
           <el-dialog title="添加用户" :visible.sync="dialogFormVisible">
@@ -50,14 +74,27 @@ export default{
         return {
             tableData:[], 
             dialogFormVisible: false,
-            username: '',
-            sex: '',
-            pass: '',
+            dialogformVisible:false,
+            username:'',
+            sex:'',
+            pass:'',
+            name:'',
+            sexy:'',
+            password:'',
+            id:'',
         }
     },
     methods:{
         getadd() {
             this.dialogFormVisible = true;
+        },
+        getUpdate(row) {
+            this.dialogformVisible = true;
+            console.log(row);
+            this.name = row.username;
+            this.sexy = row.sex;
+            this.password = row.pass;
+            this.id = row.id;
         },
         addSuc() {
             this.dialogFormVisible = false;
@@ -76,6 +113,18 @@ export default{
             data.append('id',id);
             // console.log(row.id);
             http.post('/sql/delete',data);
+            this.getData();
+            location.reload();
+        },
+        updateUser(){
+            this.dialogFormVisible = false;
+            let username = this.name, sex = this.sexy, pass = this.password , id = this.id;
+            let data = new FormData();
+            data.append('username', username);
+            data.append('sex', sex);
+            data.append('pass', pass);
+            data.append('id', id);
+            http.post('/sql/update', data);
             this.getData();
             location.reload();
         },
@@ -98,4 +147,8 @@ export default{
     position: fixed;
     bottom: 80px;
 }
+// .tablestyle{
+//     height: 408px;
+//     text-align: center;
+// }
 </style>
