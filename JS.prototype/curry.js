ead1ded589fc7ef5dff58715e9c15b7a1f84ccbb
scurrypf <2549018667@qq.com>
2023-08-21 00:04:50 +0800

    curry

--- JS.prototype/curry.js
+++ JS.prototype/curry.js
@@ -1,12 +1,13 @@
 // 函数柯里化
 function curry(fn){
-    return function(...args){
-        if(args.length >= fn.length){
-            return fn.apply(this, args)
+    return function curried(...args1){
+        if(args1.length >= fn.length){
+            return fn.apply(this, args1);
         }else{
             return function(...args2){
-                return fn.apply(this, args.concat(args2))
+                return curried.apply(this, args1.concat(args2));
             }
         }
     }
 }
+
