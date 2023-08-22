ff92a795ae64d4790f42c121821496e9cf773481
scurrypf <2549018667@qq.com>
2023-08-23 00:46:59 +0800

    flat

--- JS.prototype/flat.js
+++ JS.prototype/flat.js
@@ -1,4 +1,4 @@
-let arr = [1, 2, 3, [1, 2, 3, [1, 2, 3]]]
+let arr = [1, 2, 3, [1, [2, 3, [1, 2, [3]]]]]
 
 function flat(arr){
     let res = []
@@ -12,4 +12,17 @@ function flat(arr){
     return res
 }
 
-console.log(flat(arr));
+function flatDepth(arr, depth = 1){
+    if(depth === 0)return arr.slice();
+    let res = []
+    arr.forEach((ele)=>{
+        if(Array.isArray(ele)){
+            res = res.concat(flatDepth(ele, depth - 1));
+        }else{
+            res.push(ele);
+        }
+    })
+    return res
+}
+
+console.log(flatDepth(arr, 1));
