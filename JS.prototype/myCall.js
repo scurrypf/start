8eb9a9b3e82d04c78e421621e7031ee57ffdb1a9
scurrypf <2549018667@qq.com>
2023-09-13 23:50:48 +0800

    myCall-test

--- JS.prototype/myCall.js
+++ JS.prototype/myCall.js
@@ -1,11 +1,20 @@
+function fun(a, b){
+    console.log(this, a, b);
+    return;
+}
+
 Function.prototype.myCall = function (obj,...args){//传入对象与数值
     if(this===Function.prototype){
         return undefined;
     }
-    obj = obj || window;//obj(对象)为可选参数，在没有赋值时为window
-    const fun = Symbol();//使用symbol防止有同名属性
+    obj = obj || global;//obj(对象)为可选参数，在没有赋值时为window
+    const fun = Symbol('');//使用symbol防止有同名属性
     obj[fun]=this;
     const result = obj[fun](...args);//借用该函数并执行
     delete obj[fun];//借用完函数delete掉
     return result;//返回函数执行值
 }
+
+
+fun.myCall(null, 0, 1)
+fun.myCall({d:1}, 1, 2)
